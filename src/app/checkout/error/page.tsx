24f4328a2039e8643f7711921b57b3d5a1baea4e
Giorge Abdala <giorgeabdala@gmail.com>
2024-03-19 22:45:02 -0300

    adusted layout.

--- src/app/checkout/error/page.tsx
+++ src/app/checkout/error/page.tsx
@@ -9,9 +9,10 @@ export default function checkout() {
             <div className="flex flex-col items-center justify-center  ">
                 {/* eslint-disable-next-line react/jsx-no-undef */}
                 <Image src="/logo_transparent.png" alt="logotipo da comunidade Polkadot Brasil" width={500} height={500} />
-                <h1 className="text-4xl font-bold  font-title text-polkadot-rose ">POLKADOT BRASIL</h1>
+                <h1 className="text-xl font-bold  font-title text-polkadot-rose ">POLKADOT BRASIL</h1>
             </div>
-            <div className="flex flex-col font-body items-center font-black text-justify mt-20 gap-8 text-lg text-white">
+            <div className="flex flex-col font-body items-center font-black text-justify mt-8 gap-4 text-lg text-white">
+                <div>ERRO!</div>
                 <div>
                     Houve algum erro no processamento da sua solicitação. Por favor, confira seus dados e tente novamente.
                     </div>
--- src/app/checkout/sucess/page.tsx
+++ src/app/checkout/sucess/page.tsx
@@ -9,9 +9,9 @@ export default function checkout() {
             <div className="flex flex-col items-center justify-center  ">
                 {/* eslint-disable-next-line react/jsx-no-undef */}
                 <Image src="/logo_transparent.png" alt="logotipo da comunidade Polkadot Brasil" width={500} height={500} />
-                <h1 className="text-2xl font-bold  font-title text-polkadot-rose md:text-4xl">POLKADOT BRASIL</h1>
+                <h1 className="text-xl font-bold  font-title text-polkadot-rose md:text-4xl">POLKADOT BRASIL</h1>
             </div>
-            <div className="flex flex-col font-body items-center font-black text-justify mt-20 gap-8 text-xl text-white leading-loose">
+            <div className="flex flex-col font-body items-center font-black text-justify mt-4 gap-4 text-xl text-white leading-loose">
                 <div className="font-normal">
 
                     </div>
